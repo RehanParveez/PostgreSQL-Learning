dd7252f49cd287c51e5faa5acb574025ccfab028
Rehan Parveez <rehanparveez@gmail.com>
2026-02-23 22:49:11 +0500

    this commit is about the database practice i am doing while learning postgresql working with django. firsty i did not realize that the connection was working okay, i thought the connection was not working, so when i was trying to insert the data in stores it actually kept inserting multiple times and i thought the values are not getting saved. then i got the understanding after running select * operation on stores that the connection issue was not there and the data was already there many times. then cleaned the database related to stores and products. then inserted three stores values again and in them added the products related to each store id which were created earlier. further practiced a query related to join which was related to combining the products with their related stores

--- BasicsDatabase/settings.py
+++ BasicsDatabase/settings.py
@@ -73,23 +73,23 @@ WSGI_APPLICATION = 'BasicsDatabase.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#      'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres_practice', 
-#         'USER': 'postgres',              
-#         'PASSWORD': 'rehpostgre1',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#      }
-#  }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres_practice', 
+        'USER': 'postgres',              
+        'PASSWORD': 'rehpostgre1',
+        'HOST': 'localhost',
+        'PORT': '5432',
+     }
+ }
 
 
 # Password validation
--- practice_local.session.sql
+++ practice_local.session.sql
@@ -1,11 +1,5 @@
-CREATE TABLE stores(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(40) NOT NULL,
-    city VARCHAR(40) NOT NULL,
-    total_products INT
-);
-
-INSERT INTO stores (name, city, total_products) VALUES
-('Tech Store', 'Multan', 25),
-('General Store', 'Bahawalpur', 30),
-('Toys Store', 'Lahore', 20);
+SELECT
+     products.name AS product_name,
+     stores.name AS store_name
+FROM products
+JOIN stores ON products.store_id = stores.id;
