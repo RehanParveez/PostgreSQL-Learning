2249724087543047ba195899d2d42e48df5a7ee8
Rehan Parveez <rehanparveez@gmail.com>
2026-02-24 23:53:36 +0500

    this commit is about the practice of few more sql queires

--- practice_local.session.sql
+++ practice_local.session.sql
@@ -77,11 +77,42 @@
 
 -- show average price of products per store
 
-SELECT
-      stores.name AS store_name,
-      AVG(products.price) AS avg_price
+-- SELECT
+--       stores.name AS store_name,
+--       AVG(products.price) AS avg_price
+-- FROM stores
+-- JOIN products ON stores.id = products.store_id
+-- GROUP BY stores.id, stores.name;
+
+
+-- Show products from Lahore store only
+
+-- SELECT products.name,
+--                      stores.city              
+-- FROM products
+-- JOIN stores ON products.store_id = stores.id
+-- WHERE stores.city = 'Multan';
+
+
+-- Stores with more than 2 products
+
+-- SELECT stores.name,
+--                    COUNT(products.id) AS product_count
+-- FROM stores
+-- JOIN products ON stores.id = products.store_id
+-- GROUP BY stores.id, stores.name
+-- HAVING COUNT(products.id) > 2;
+
+
+-- Products with price > 5000, grouped per store, having avg price > 25000
+
+SELECT stores.name,
+                   AVG(products.price) AS avg_price
 FROM stores
 JOIN products ON stores.id = products.store_id
-GROUP BY stores.id, stores.name;
+WHERE products.price > 5000
+GROUP BY stores.id, stores.name
+HAVING AVG(products.price) > 25000;
+
 
 
